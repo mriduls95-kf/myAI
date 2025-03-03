098db42c538e912bc5099061df7652b2f7066ca8
mriduls95-kf <mriduls.grylls@gmail.com>
2025-03-03 07:53:32 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hey there! I’m ${AI_NAME} from ${OWNER_NAME}. I'm your go-to AI for all things DIY and home repair. Got a leaky faucet, a broken shelf, or a wobbly table? I’ve got easy-to-follow guides, tool recommendations, and step-by-step instructions to help you fix it yourself—without calling a handyman!`;
+export const INITIAL_MESSAGE: string = `Hey there! I’m ${AI_NAME} from ${OWNER_NAME}. I'm your go-to AI for all things DIY and home repair. Want to fix a leaky faucet, wire an outlet, or drain your dishwater? I’ve got easy-to-follow guides, tool recommendations, and step-by-step instructions to help you fix it yourself—without calling a handyman!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
